b163fd9ca42f5b62c7e1fe8ef4e43bf8d93d6a54
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2025-06-03 17:22:16 -0300

    feat: header maior

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,7 +27,7 @@
 </script>
 
 <div class="flex flex-col items-center py-12">
-  <h1 class="py-3 text-xl text-white">Assitente virtual para emergências</h1>
+  <h1 class="py-4 text-2xl text-white">Assitente virtual para emergências</h1>
   <div class="w-[50rem] rounded-t-xl border-1 bg-white p-4">
     {#if history.length == 0}
       <p>Como posso te ajudar?</p>
